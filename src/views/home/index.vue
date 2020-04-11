dcf647f8f7959801a99a01c69482d02e257c3aee
ZhangYaoyv <2196878189@qq.com>
2020-04-11 15:16:21 +0800

    登录页搭建完成

---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="home">主页
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="login">
+      <el-card class="login-cread">
+        <div class="tu">
+          <img src="../../assets/img/keai.gif" alt="" class="photo">
+        </div>
+        <!-- 表单 -->
+        <el-form>
+          <el-form-item>
+              <el-input placeholder="手机号"></el-input>
+          </el-form-item>
+          <el-form-item>
+              <el-input placeholder="验证码" style="width:60%"></el-input>
+              <el-button style="float:right" plain>默认按钮</el-button>
+          </el-form-item>
+          <el-form-item style="margin-top:-15px">
+              <el-checkbox>必须同意我们欺负你才带你玩</el-checkbox>
+          </el-form-item>
+          <el-form-item style="margin-top:-15px">
+              <el-button type="danger" style="width:100%">主要按钮</el-button>
+          </el-form-item>
+        </el-form>
+
+      </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style scoped lang="less">
+.login{
+  background-image: url("../../assets/img/gugong1.jpg");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-cread{
+    width: 440px;
+    height: 350px;
+
+  }
+}
+.tu{
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .photo{
+  width: 130px;
+  height: 100px;
+
+}
+}
+
+</style>
